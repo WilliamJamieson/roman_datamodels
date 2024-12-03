6a10a34dd974072fc04405fad84d56e8909f1608
William Jamieson <william@jamieson.phd>
2024-12-02 22:13:43 -0500

    Add mosaic source catalog

---
+++ src/roman_datamodels/stnode/_nodes/datamodels/mosaic_source_catalog.py
@@ -0,0 +1,63 @@
+from astropy.table import Table
+
+from roman_datamodels.stnode import _core
+
+from ..meta import (
+    Basic,
+    MosaicBasic,
+    Photometry,
+    Program,
+)
+
+__all__ = ["MosaicSourceCatalog"]
+
+
+class MosaicSourceCatalogMeta(Basic):
+    @property
+    def required(self) -> tuple[str]:
+        return (
+            "basic",
+            "photometry",
+            "program",
+        )
+
+    @property
+    def basic(self) -> MosaicBasic:
+        return self._get_node("basic", MosaicBasic)
+
+    @property
+    def photometry(self) -> Photometry:
+        return self._get_node("photometry", Photometry)
+
+    @property
+    def program(self) -> Program:
+        return self._get_node("program", Program)
+
+
+class MosaicSourceCatalog(_core.DataModelNode):
+    """
+    Photometry and astrometry computed by the Source Catalog Step
+    """
+
+    @property
+    def tag(self) -> str:
+        return "asdf://stsci.edu/datamodels/roman/tags/mosaic_source_catalog-1.0.0"
+
+    @property
+    def required(self) -> tuple[str]:
+        return (
+            "meta",
+            "source_catalog",
+        )
+
+    @property
+    def array_shape(self) -> tuple[int]:
+        raise NotImplementedError("This node does not have an array shape")
+
+    @property
+    def meta(self) -> MosaicSourceCatalogMeta:
+        return self._get_node("meta", MosaicSourceCatalogMeta)
+
+    @property
+    def source_catalog(self) -> Table:
+        return self._get_node("source_catalog", lambda: Table([range(3), range(3)], names=["a", "b"]))
