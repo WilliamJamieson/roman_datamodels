Fixes for removal of units from ref files

src/roman_datamodels/nodes/reference_files/dark.py
@@ -1,7 +1,6 @@
 from types import MappingProxyType
 
 import numpy as np
-from astropy import units as u
 
 from roman_datamodels.stnode import rad
 
@@ -31,18 +30,6 @@ class DarkRef_Meta_Exposure(RefExposureTypeRef_Exposure, rad.ImpliedNodeMixin):
             *super().asdf_required(),
             *RefExposureTypeRef_Exposure.asdf_required(),
         }
-
-    @rad.field
-    def ngroups(self) -> int:
-        return 6
-
-    @rad.field
-    def nframes(self) -> int:
-        return 8
-
-    @rad.field
-    def groupgap(self) -> int:
-        return 0
 
     @rad.field
     def ma_table_name(self) -> str:
@@ -101,17 +88,17 @@ class DarkRef(rad.TaggedObjectNode, rad.ArrayFieldMixin):
         return DarkRef_Meta()
 
     @rad.field
-    def data(self) -> u.Quantity:
-        return u.Quantity(np.zeros(self.array_shape, dtype=np.float32), unit=u.DN, dtype=np.float32)
+    def data(self) -> np.ndarray:
+        return np.zeros(self.array_shape, dtype=np.float32)
 
     @rad.field
     def dq(self) -> np.ndarray:
         return np.zeros(self.array_shape[1:], dtype=np.uint32)
 
     @rad.field
-    def dark_slope(self) -> u.Quantity:
-        return u.Quantity(np.zeros(self.array_shape[1:], dtype=np.float32), unit=u.DN / u.s, dtype=np.float32)
+    def dark_slope(self) -> np.ndarray:
+        return np.zeros(self.array_shape[1:], dtype=np.float32)
 
     @rad.field
-    def dark_slope_error(self) -> u.Quantity:
-        return u.Quantity(np.zeros(self.array_shape[1:], dtype=np.float32), unit=u.DN / u.s, dtype=np.float32)
+    def dark_slope_error(self) -> np.ndarray:
+        return np.zeros(self.array_shape[1:], dtype=np.float32)

src/roman_datamodels/nodes/reference_files/readnoise.py
@@ -1,7 +1,6 @@
 from types import MappingProxyType
 
 import numpy as np
-from astropy import units as u
 
 from roman_datamodels.stnode import rad
 
@@ -50,5 +49,5 @@ class ReadnoiseRef(rad.TaggedObjectNode, rad.ArrayFieldMixin):
         return ReadnoiseRef_Meta()
 
     @rad.field
-    def data(self) -> u.Quantity:
-        return u.Quantity(np.zeros(self.array_shape, dtype=np.float32), u.DN, dtype=np.float32)
+    def data(self) -> np.ndarray:
+        return np.zeros(self.array_shape, dtype=np.float32)