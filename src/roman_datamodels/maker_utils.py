bca2ab07e7929c716c67d8ffb7ee7fb40765531a
William Jamieson <william@jamieson.phd>
2025-01-30 15:08:13 -0500

    Update warning message

--- src/roman_datamodels/maker_utils.py
+++ src/roman_datamodels/maker_utils.py
@@ -4,7 +4,8 @@ from ._maker_utils import *  # noqa: F403
 
 _MSG = """
 Use of `roman_datamodels.maker_utils` is deprecated and will be removed in the
-future! Please use the `.create_default()` constructor for the datamodel that
-you wish to make a default datamodel for.
+future! Please simply initialize the model you want to use directly with no arguments,
+e.g. `ImageModel()`. The model will then fill in the default values automatically
+if you require them.
 """
 warnings.warn(_MSG, DeprecationWarning, stacklevel=2)
